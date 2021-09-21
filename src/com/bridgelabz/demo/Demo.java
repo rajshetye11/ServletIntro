837a6d2f8b5838b8f383697313572754415f7c7d
rajshetye11 <rajshetye11@gmail.com>
2021-09-21 20:14:12 +0530

    [raj] : added initial setup

---
+++ src/com/bridgelabz/demo/Demo.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Demo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public Demo() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out= response.getWriter();
+		out.print("<h1>hello Servlet<h1>");
+	}
+
+
+}
